644b17b416c510e69be2e2915963b3a39fea3b95
Jannettys24 <118224718+Jannettys24@users.noreply.github.com>
2023-03-03 16:10:19 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import data from './data';
+import { Route, Routes } from "react-router-dom";
+import Homepage from './component/Homepage';
+import Dashboard from './component/Dashboard';
+import About from './component/About';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Details from './page/Details';
 
 function App() {
+  const [stock,setStock] = useState(data);
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>stocks</h1> */}
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Homepage />}/> 
+         <Route path='/about' element={<About />} />
+        <Route path='/stocks' element={<Dashboard stock={stock}/>}/>
+        <Route path='/stock/:symbol'  element={<Details stock={stock}/>} />
+
+      </Routes>
+     <Footer />
+      
     </div>
   );
 }
